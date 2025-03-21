3977f941eabb452cde7c80a67aefc47fa78659c1
Shotix <72971195+Shotix@users.noreply.github.com>
2025-03-21 11:04:14 +0100

    Added a default constructor

--- src/data/ApiErrorResponse.ts
+++ src/data/ApiErrorResponse.ts
@@ -3,9 +3,12 @@ import {ApiErrorDetails} from "@/data/interfaces";
 export class ApiErrorResponse {
     constructor(
         public message: string,
-        public statusCode: number,
-        public instance: string,
-        public errors: ApiErrorDetails[]
+        public statusCode?: number,
+        public instance?: string,
+        public errors?: ApiErrorDetails[]
     ) {
+        this.statusCode = statusCode || 500;
+        this.instance = instance || "";
+        this.errors = errors || [];
     }
 }
