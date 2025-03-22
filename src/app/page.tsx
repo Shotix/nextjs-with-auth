786a4f6236851b4addafa82a92bf09b488844258
Shotix <72971195+Shotix@users.noreply.github.com>
2025-03-22 18:31:15 +0100

    Stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-"use client";
+import React from "react";
 
-export default function Home() {
-
+const Dashboard: React.FC = () => {
     return (
-        <>
-            <p>Dashboard</p>
-        </>
-    );
+        <p>
+            Welcome to your dashboard!
+        </p>
+    )
 }
+
+export default Dashboard;
