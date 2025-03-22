7b4d2965ce143ba978ec664268e06167d66e7c56
Shotix <72971195+Shotix@users.noreply.github.com>
2025-03-22 18:30:32 +0100

    Added "App" from antd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,9 +8,11 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient } from "@tanstack/query-core";
 import { UserProvider } from "@/contexts/UserContext";
-import Navbar from "@/app/components/ui/Navbar";
+import Navbar from "@/components/custom/Navbar";
 import { Inter } from 'next/font/google'
 import {usePathname} from "next/navigation";
+import '@ant-design/v5-patch-for-react-19';
+import {App} from "antd";
 
 const queryClient = new QueryClient();
 const inter = Inter({ subsets: ['latin'] })
@@ -27,26 +29,28 @@ export default function RootLayout({
     return (
         <html lang="en" className={inter.className}>
         <body>
-        <AuthProvider>
-            <ProtectedRoute>
-                <QueryClientProvider client={queryClient}>
-                    {(isLoginPage || isRegisterPage) && (
-                        <main>
-                            {children}
-                        </main>
-                    )}
-                    {(!isLoginPage && !isRegisterPage) && (
-                        <UserProvider>
-                            <div className="app-container">
-                                <Navbar />
-                                <main>{children}</main>
-                            </div>
-                        </UserProvider>
-                    )}
-                    <ReactQueryDevtools initialIsOpen={false} />
-                </QueryClientProvider>
-            </ProtectedRoute>
-        </AuthProvider>
+        <App>
+            <AuthProvider>
+                <ProtectedRoute>
+                    <QueryClientProvider client={queryClient}>
+                        {(isLoginPage || isRegisterPage) && (
+                            <main>
+                                {children}
+                            </main>
+                        )}
+                        {(!isLoginPage && !isRegisterPage) && (
+                            <UserProvider>
+                                <div className="app-container">
+                                    <Navbar />
+                                    <main>{children}</main>
+                                </div>
+                            </UserProvider>
+                        )}
+                        <ReactQueryDevtools initialIsOpen={false} />
+                    </QueryClientProvider>
+                </ProtectedRoute>
+            </AuthProvider>
+        </App>
         </body>
         </html>
     );
